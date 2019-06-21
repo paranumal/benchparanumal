b0e8c5b6f2e15efb25863d92c7ef5c4578e87bed
Tim Warburton <tim.warburton@gmail.com>
2019-06-21 14:25:49 -0400

    WIP: addding Radeon VII tuned BP5 kernel

--- CEED/BP/occa/src/BPSolve.cpp
+++ CEED/BP/occa/src/BPSolve.cpp
@@ -118,7 +118,7 @@ int BPPCG(BP_t* BP, dfloat lambda,
     // p = z + beta*p
     BPScaledAdd(BP, 1.f, o_z, beta, o_p);
 
-#if 1
+#if 0
     // Ap
     BPOperator(BP, lambda, o_p, o_Ap, dfloatString); 
    
